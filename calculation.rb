8d3915b521a8661b03c2a12034b5414887ba92e4
Stacy Andrews <stacy@cribber.com>
2019-05-01 18:38:37 +0800

    applied module so Calculation functions can be mixed-in

--- calculation.rb
+++ calculation.rb
@@ -1,59 +1,60 @@
 require 'humanize'
 
+module Calculation
+  (0..9).each do |number|
+    define_method number.humanize do |calculation=Calculation.new|
+      calculation.apply number
 
-(0..9).each do |number|
-  define_method number.humanize do |calculation=Calculation.new|
-    calculation.apply number
+      return calculation.calc if calculation.can_calc?
 
-    return calculation.calc if calculation.can_calc?
-
-    calculation
-  end
-end
-
-OPERATIONS = {
-  times: lambda { |left, right| left*right },
-  plus: lambda { |left, right| left+right },
-  minus: lambda { |left, right| left-right },
-  divided_by: lambda { |left, right| left/right }
-}
-
-OPERATIONS.each do |key, operation|
-  define_method key.to_s do |op|
-    op.apply_operator(operation)
-    op
-  end
-end
-
-class Calculation
-  def initialize
-    self.left = -1
-    self.right = -1
+      calculation
+    end
   end
 
-  def calc
-    operator.call left, right
+  OPERATIONS = {
+    times: lambda { |left, right| left*right },
+    plus: lambda { |left, right| left+right },
+    minus: lambda { |left, right| left-right },
+    divided_by: lambda { |left, right| left/right }
+  }
+
+  OPERATIONS.each do |key, operation|
+    define_method key.to_s do |op|
+      op.apply_operator(operation)
+      op
+    end
   end
 
-  def apply(number)
-    return self.right = number if not_set?(right)
+  class Calculation
+    def initialize
+      self.left = -1
+      self.right = -1
+    end
 
-    self.left = number if not_set?(left)
-  end
+    def calc
+      operator.call left, right
+    end
 
-  def apply_operator(operation)
-    @operator = operation
-  end
+    def apply(number)
+      return self.right = number if not_set?(right)
 
-  def can_calc?
-    left >=0 && right >=0
-  end
+      self.left = number if not_set?(left)
+    end
 
-  private
+    def apply_operator(operation)
+      @operator = operation
+    end
 
-  attr_accessor :left, :right, :operator
+    def can_calc?
+      left >=0 && right >=0
+    end
 
-  def not_set?(value)
-    value == -1
+    private
+
+    attr_accessor :left, :right, :operator
+
+    def not_set?(value)
+      value == -1
+    end
   end
 end
--- spec/calculation_spec.rb
+++ spec/calculation_spec.rb
@@ -2,22 +2,32 @@ require_relative '../calculation'
 
 describe 'Calculation (Split Payments Task 1)' do
   it 'can run one times example' do
+    extend Calculation
+
     expect(six(times(five))).to eq(30)
   end
 
   it 'can run one plus example' do
+    extend Calculation
+
     expect(one(plus(nine))).to eq(10)
   end
 
   it 'can run one minus example' do
+    extend Calculation
+
     expect(six(minus(three))).to eq(3)
   end
 
   it 'can run one divide_by example' do
+    extend Calculation
+
     expect(eight(divided_by(two))).to eq(4)
   end
 
   it 'divide_by zero should error' do
+    extend Calculation
+
     expect { eight(divided_by(zero)) }.to raise_error(ZeroDivisionError)
   end
 end
